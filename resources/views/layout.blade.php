bbd6c310708cf8831846f482d0161b24d7f1e8e8
chuan-hao, you <howtomakeaturn@hotmail.com>
2016-11-26 14:56:13 +0800

    add GA

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+        <title></title>
+
+        <meta name="description" content="">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name='og:title' content="">
+
+        <meta name='og:description' content="">
+
+        <meta name='og:image' content="">
+
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+
+        <link rel="stylesheet" href="/css/font-awesome.min.css">
+
+        <script src="/js/jquery-1.12.0.min.js"></script>
+
+        <script src='/js/bootstrap.min.js'></script>
+
+    </head>
+    <body>
+        @yield('content')
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-85456048-3', 'auto');
+          ga('send', 'pageview');
+
+        </script>
+    </body>
+</html>
