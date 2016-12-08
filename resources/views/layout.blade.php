cced2e15874ec5ad7a79ca82971ddbde5f244351
chuan-hao, you <howtomakeaturn@hotmail.com>
2016-12-08 15:39:20 +0800

    ui

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -30,6 +30,44 @@
     </head>
     <body>
 
+        <nav class="navbar navbar-inverse">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="/">做網站要多少錢</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li><a href="/">預算說明清單</a></li>
+                <li><a href="/dictionary">給業主參考的術語辭典</a></li>
+
+              <!--
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+            -->
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+
         <div class="container">
 
             <div class='row'>
@@ -44,20 +82,12 @@
                 </div>
             </div>
 
-            <div class="row">
-                <ul class="nav nav-pills">
-                  <li role="presentation"><a href="/">預算說明清單</a></li>
-                  <li role="presentation"><a href="/dictionary">給業主參考的術語辭典</a></li>
-                </ul>
-            </div>
-            <br>
-
             <div class='row hidden-xs'>
                 <div class='col-md-6'>
                     <ul class="list-group">
+                      <li class="list-group-item">於 2016-12-08 15:38:27 更新：UI</li>
                       <li class="list-group-item">於 2016-12-04 21:30:38 更新：項目資料</li>
                       <li class="list-group-item">於 2016-12-04 15:58:01 新增：給業主參考的術語辭典</li>
-                      <li class="list-group-item">於 2016-11-26 22:41:28 更新：項目資料</li>
                     </ul>
                 </div>
                 <div class='col-md-6'>
