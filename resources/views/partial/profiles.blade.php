dc4890d1f8b4e55bd4faf1efd029d04a61d3bd97
chuan-hao, you <howtomakeaturn@hotmail.com>
2017-11-16 16:32:25 +0800

    update

--- resources/views/partial/profiles.blade.php
+++ resources/views/partial/profiles.blade.php
@@ -10,7 +10,7 @@
         <div class='col-md-6'>
 
             <div class="alert alert-success" role="alert">
-                <strong>上次更新資料時間</strong>：2017-11-16 09:03</a>
+                <strong>上次更新資料時間</strong>：2017-11-16 16:31</a>
             </div>
 
         </div>
