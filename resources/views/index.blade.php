6387e71dc3059ac537d2bdee31e5d3da84f94242
chuan-hao, you <howtomakeaturn@hotmail.com>
2017-11-17 20:50:19 +0800

    disqus

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -302,6 +302,39 @@
     </div>
 </div>
 
+<div class="bg">
+    <div class="title">
+        留言板
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div id="disqus_thread"></div>
+                <script>
+
+                /**
+                *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+
+                var disqus_config = function () {
+                this.page.url = 'https://makewebsites.tw';  // Replace PAGE_URL with your page's canonical URL variable
+                this.page.identifier = 'homepage'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                };
+
+                (function() { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                s.src = 'https://EXAMPLE.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+                })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
+            </div>
+        </div>
+    </div>
+</div>
+
 <a href="#top" id='return-to-top'><i class="fa fa-chevron-up" aria-hidden="true"></i></a>
 
 <style>
