f55ab971e112a2c73b0d2c0860694ed87e72fb74
howtomakeaturn <howtomakeaturn@hotmail.com>
2017-12-13 20:24:21 +0800

    remove old routes

--- routes/web.php
+++ routes/web.php
@@ -14,38 +14,3 @@
 Route::get('/', function () {
     return view('index');
 });
-
-Route::get('/prices', function () {
-    return redirect()->to('/');
-
-    $rows = get_rows();
-    return view('prices', ['rows' => $rows]);
-});
-
-Route::get('/dictionary', function () {
-    return redirect()->to('/');
-
-    $rows = get_dict_rows();
-    return view('dictionary', ['rows' => $rows]);
-});
-
-Route::get('/services', function () {
-    return redirect()->to('/');
-
-    $rows = get_service_rows();
-    return view('services', ['rows' => $rows]);
-});
-
-Route::get('/profiles', function () {
-    return redirect()->to('/');
-
-    $rows = get_profile_rows();
-    return view('profiles', ['rows' => $rows]);
-});
-
-Route::get('/community', function () {
-    return redirect()->to('/');
-
-    $rows = get_community_rows();
-    return view('community', ['rows' => $rows]);
-});
