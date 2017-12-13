d5714aa4849b9af96a1bc80c8f29feee91e4c58a
howtomakeaturn <howtomakeaturn@hotmail.com>
2017-12-13 20:41:05 +0800

    add github link

--- resources/views/_navbar.blade.php
+++ resources/views/_navbar.blade.php
@@ -8,24 +8,13 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <!--
-      <a class="navbar-brand" href="/">做網站要多少錢</a>
-      -->
+
       <a class="navbar-brand" href="/">{{short_title()}}</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <!--
-        <li><a href="/">Home</a></li>
-        -->
-
-        <!--
-        <li><a href="/services">國內外架站服務</a></li>
-        <li><a href="/profiles">接案公司清單</a></li>
-        <li><a href="/prices">功能與價格說明</a></li>
-        -->
 
         <li><a href="#services">架站服務</a></li>
         <li><a href="#profiles">接案公司</a></li>
@@ -33,21 +22,12 @@
         <li><a href="#community">社群討論</a></li>
         <li><a href="#discuss">留言板</a></li>
 
-      <!--
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
       </ul>
-    -->
+
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="https://github.com/howtomakeaturn/makewebsites" target="_blank"><i class="fa fa-github fa-lg" aria-hidden="true"></i>&nbsp; Github</a></li>
+      </ul>
+
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
