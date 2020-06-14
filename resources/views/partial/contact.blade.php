45f11ec7a43eb9c6d4b4d3ef6ea25250f4d43495
howtomakeaturn <howtomakeaturn@hotmail.com>
2020-06-14 18:13:29 +0800

    info

--- resources/views/partial/contact.blade.php
+++ resources/views/partial/contact.blade.php
@@ -1,9 +1,8 @@
 <div class='row'>
     <div class='col-md-8 col-md-offset-2'>
         <div class="alert alert-success" style="margin-bottom: 0; text-align: center;">
-            <h4><b>專業顧問服務</b></h4>
-            <p>您可以付費跟站長阿川在線上聊個30分鐘。</p>
-            <p>站長阿川會給您業界工程師的專業建議：<a href="https://devs.tw/talk" target="_blank" class="alert-link">請點這裡</a></p>
+            <h4><b>本站由站長阿川維護</b></h4>
+            <p>有任何商業合作洽詢，可以 FB 找站長阿川聊聊。<a class="alert-link" href="https://www.facebook.com/chuanhao.you">https://www.facebook.com/chuanhao.you</a></p>
         </div>
     </div>
 </div>
